daec9d7809f7fbfdecd07f25b6b33ce3dc4537f2
Vladyslav Sulyma <sulymavladyslav@gmail.com>
2025-08-03 18:39:47 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export const goods = [
 ];
 
 export const App = () => {
-  const [selectedGood, setValue] = useState(goods.indexOf('Jam'));
+  const [selectedGood, setValue] = useState('Jam');
 
   return (
     <main className="section container">
@@ -31,7 +31,7 @@ export const App = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setValue(null)}
+            onClick={() => setValue('')}
           />
         </h1>
       )}
